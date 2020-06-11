e4f98a5e64fe2478deccd0b0277714cf57e04f64
Bryan Houlton <brhoulton@gmail.com>
2020-06-11 11:18:46 -0400

    added frontend login page

--- client/client.js
+++ client/client.js
@@ -1,1 +1,19 @@
-console.log("HELLO");
+
+var startbutton = $("#startbutton");
+
+
+startbutton.click(function() {
+  $("#first").fadeOut("swing");
+  setTimeout(present_login, 1000);
+})
+
+function present_login(){
+  $("#login-form").fadeIn();
+  console.log("2");
+}
+
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
