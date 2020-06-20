1bdb2e13ce4df07096439fb50dd272f7d12a273c
Bryan Houlton <brhoulton@gmail.com>
2020-06-20 13:02:22 -0400

    fixing tree

--- server/pages/dash/dash.js
+++ server/pages/dash/dash.js
@@ -4,14 +4,3 @@ $("#colors").click(function() {
   rotate += 20;
   document.querySelector('html').style.filter = 'hue-rotate('+String(rotate)+'deg)'
 });
-
-$("#newgoalbutton").click(function() {
-    var w = $(".dash-content").width() + 72;
-    $("#new-goal").css({left:202})
-});
-
-$("#minimize-button").click(function() {
-    $("#new-goal").css({left:'-80%'})
-});
-
-window.onresize = goals_resize;
